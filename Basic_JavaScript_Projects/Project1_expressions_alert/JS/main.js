8eaa98fa8bc6d368ff611330229b35231cce0344
codyharris713 <83795096+codhharris713@users.noreply.github.com>
2021-06-16 12:09:36 -0700

    two variables concatenate

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -3,16 +3,24 @@ var A = "This is a String";
 document.write(A);
 var B = "This is B";
 window.alert(B);
-document.write("\"Be who yu are and say what feel,"
-+" because those who mide dont\'t matter and those who matter dont\'t mind.\""
+document.write("\"Be who you are and say what feel,"
++" because those who middlee dont\'t matter and those who matter dont\'t mind.\""
 + "-Dr. Seuss");
 var Family = "The Family Matters", Dad = "Carl", Mom = "Harriette",
                 Daughter = "Estelle", Son = "Edward";
 document.write(Dad);
+
+var c = "yourname";
+var d = "yourlastname";
+var name = c + d;
+document.write(c + d);
+
 var red = "This is the color red.";
 var red = red.fontcolor("red");
 document.write(red);
+
 document.write(4 + 4);
+
 function My_First_Function() { //Defining a function and naming it
     var str = "This text is green!"; //Defining a variable and giving it a string value
     var result = str.fontcolor ("green"); //Using the fontcolor method on str variable
