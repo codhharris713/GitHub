7ee08f4957ed3e73297e63daf01643106513fe2a
codyharris713 <83795096+codhharris713@users.noreply.github.com>
2021-06-16 14:04:01 -0700

    operator

---
+++ Basic_JavaScript_Projects/Project2_Functions/JS/main.js
@@ -0,0 +1,10 @@
+function another_function() { //defined and named function
+    var str = "something"; //defined a variable with string value 
+    var result = str.fontcolor("blue"); // fontcolor methond
+    document.getElementById("blue_text").innerHTML = result; //value result on HTML id blue_text
+}
+function myFunction() { //declared function
+    var sentence = "I am learning"; //createtd variable called sentence assign value to I am learing
+    sentence += "a lot from this book!"; //used += operator to concatenate a string 
+    document.getElementById("Concatnate").innerHTML = sentence;
+}
