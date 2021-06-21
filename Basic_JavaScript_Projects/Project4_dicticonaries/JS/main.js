52dc82df437377e45a6723929bee910a44cd256e
codyharris713 <83795096+codhharris713@users.noreply.github.com>
2021-06-20 20:24:48 -0700

    new commit

---
+++ Basic_JavaScript_Projects/Project4_dicticonaries/JS/main.js
@@ -0,0 +1,16 @@
+var person = { // create object
+    firstName: "Cody",
+    lastName: "Harris",
+    age: "34",
+    eyeColor: "green"
+};
+
+function something() {
+document.getElementById ("Dictionary").innerHTML = //Display some data from the object
+person.firstName + "is" + person.age + "years old.";
+}
+
+function something() {
+delete person.Cody; 
+document.getElementById ("Dictionary").innerHTML = person.Cody; // delete key
+}
